26e63b49aa746ca0e1b4f4d28db14336a64975b7
asy-init <94440862+asy-init@users.noreply.github.com>
2022-01-05 07:39:07 +0530

    File restructre complete

---
+++ asymemtral/__main__.py
@@ -0,0 +1,4 @@
+import hashlib
+import sqlite3
+
+import click
---
+++ setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup
+
+setup(
+    name="asymmetral", description="", long_description="", author="", license="GNU v2"
+)
